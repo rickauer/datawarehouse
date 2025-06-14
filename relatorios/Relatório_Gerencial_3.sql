8ed4e1031a57ebc3cd0bcaefc6d06a901165dd34
rickauer <116601341+rickauer@users.noreply.github.com>
2025-06-14 12:10:03 -0300

    Update Relatório_Gerencial_3.sql

--- relatorios/Relatório_Gerencial_3.sql
+++ relatorios/Relatório_Gerencial_3.sql
@@ -1,3 +1,9 @@
+-- Guilherme Oliveira Rolim Silva - DRE: 122076696
+
+-- Ricardo Lorente Kauer - DRE: 122100500
+
+-- Vinícius Alcântara Gomes Reis de Souza - DRE: 122060831
+
 SELECT
     dg.nome_grupo AS grupo_do_veiculo,
     dc.cidade_cliente,
