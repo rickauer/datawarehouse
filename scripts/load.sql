cdfe6ef781bc60af69a3238c02ca424a3501428b
Guilherme Rolim <100958863+rolim520@users.noreply.github.com>
2025-06-14 12:10:27 -0300

    Update load.sql

--- scripts/load.sql
+++ scripts/load.sql
@@ -1,3 +1,9 @@
+-- Guilherme Oliveira Rolim Silva - DRE: 122076696
+
+-- Ricardo Lorente Kauer - DRE: 122100500
+
+-- Vinícius Alcântara Gomes Reis de Souza - DRE: 122060831
+
 INSERT INTO dwh.Dim_Tempo (data, dia, mes, ano, trimestre, nome_mes, dia_da_semana)
 SELECT datum,
        EXTRACT(DAY FROM datum),
