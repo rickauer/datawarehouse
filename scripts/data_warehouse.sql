b61bd72db74d9b07b858b56786fb101c6300a9de
Guilherme Rolim <100958863+rolim520@users.noreply.github.com>
2025-06-14 12:10:13 -0300

    Update data_warehouse.sql

--- scripts/data_warehouse.sql
+++ scripts/data_warehouse.sql
@@ -1,3 +1,9 @@
+-- Guilherme Oliveira Rolim Silva - DRE: 122076696
+
+-- Ricardo Lorente Kauer - DRE: 122100500
+
+-- Vinícius Alcântara Gomes Reis de Souza - DRE: 122060831
+
 CREATE SCHEMA dwh;
 
 -- Tabela de Dimensão de Tempo
