ef40776671481f5464585d840c066d5df6e68d69
rickauer <116601341+rickauer@users.noreply.github.com>
2025-06-14 12:09:34 -0300

    Update Matriz_Percentuais.sql

--- relatorios/Matriz_Percentuais.sql
+++ relatorios/Matriz_Percentuais.sql
@@ -1,3 +1,9 @@
+-- Guilherme Oliveira Rolim Silva - DRE: 122076696
+
+-- Ricardo Lorente Kauer - DRE: 122100500
+
+-- Vinícius Alcântara Gomes Reis de Souza - DRE: 122060831
+
 WITH Movimentacoes AS (
     -- Conta o número de viagens entre cada par de pátios
     SELECT
